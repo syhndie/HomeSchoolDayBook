b5bc1eeb9368c399a3585605d78b90812229614b
Cindy <syhndie2@gmail.com>
2018-10-08 20:21:16 -0500

    Added DbSets corresponding to the data models to DbContext

--- HomeSchoolDayBook/Data/ApplicationDbContext.cs
+++ HomeSchoolDayBook/Data/ApplicationDbContext.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using HomeSchoolDayBook.Models;
 
 namespace HomeSchoolDayBook.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Entry> Entries { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<SubjectAssignment> SubjectAssignments { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
