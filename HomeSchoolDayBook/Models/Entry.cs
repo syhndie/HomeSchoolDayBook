607503ad2d4d879ef9762430f9ced7c30d782c12
Cindy <syhndie2@gmail.com>
2018-10-09 13:45:32 -0500

    added date property to entry model

--- HomeSchoolDayBook/Models/Entry.cs
+++ HomeSchoolDayBook/Models/Entry.cs
@@ -9,6 +9,7 @@ namespace HomeSchoolDayBook.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        public DateTime Date { get; set; }
         public int MinutesSpent { get; set; }
         public string Description { get; set; }
 
