088a95853bdeb18d9e0fcef2d64ffb456e02af22
Cindy <syhndie2@gmail.com>
2018-10-10 20:33:53 -0500

    Updated Entry model for display of TimeSpent. Also updated Delete, Details and Index pages for Entry model.

--- HomeSchoolDayBook/Models/Entry.cs
+++ HomeSchoolDayBook/Models/Entry.cs
@@ -15,6 +15,36 @@ namespace HomeSchoolDayBook.Models
         public DateTime Date { get; set; }
 
         public int MinutesSpent { get; set; }
+
+        public int ComputesHours
+        {
+            get
+            {
+                return MinutesSpent / 60;
+            }
+        }
+
+        public int ComputedMinutes
+        {
+            get
+            {
+                return MinutesSpent % 60;
+            }
+        }
+
+        [Display(Name = "Time Spent")]
+        public string ComputedTimeSpent
+        {
+            get
+            {
+                string hoursUnits = ComputesHours == 1 ? "hour" : "hours";
+                string minutesUnits = ComputedMinutes == 1 ? "minute" : "minutes";
+                string hours = ComputesHours == 0 ? "" : $"{ComputesHours} {hoursUnits}";
+                string minutes = ComputedMinutes == 0 ? "" : $"{ComputedMinutes} {minutesUnits}";
+                return hours == "" || minutes == "" ? $"{hours} {minutes}" : $"{hours}, {minutes}";
+            }
+        }
+
         public string Description { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
