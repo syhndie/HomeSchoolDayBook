891b7b089be32cadec842db34f1283f2c32c811b
Cindy <syhndie2@gmail.com>
2018-10-21 14:38:33 -0500

    Added datatables files and initialized datatables for Events Index page

--- HomeSchoolDayBook/wwwroot/js/site.js
+++ HomeSchoolDayBook/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+    $(document).ready(function () {
+        $('#entryindex').DataTable();
+    })
