efee2688e3e5abfbddddac020631c926c0e07916
Cindy <syhndie2@gmail.com>
2018-11-05 13:34:00 -0600

    Added IsActive property to Studen model and updated database. Add this field to Student Index page, along with Datatables formatting

---
+++ HomeSchoolDayBook/Data/Migrations/20181105192200_AddedIsActiveBoolToStudentModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeSchoolDayBook.Data.Migrations
+{
+    public partial class AddedIsActiveBoolToStudentModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Students",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Students");
+        }
+    }
+}
