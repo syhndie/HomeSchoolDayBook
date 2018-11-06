be5ae5bc2fd62dd38386652480b00a07d6fa9931
Cindy <syhndie2@gmail.com>
2018-11-06 14:08:19 -0600

    added IsActive checkbox to Student Create page and Subject Create page

--- HomeSchoolDayBook/Models/Student.cs
+++ HomeSchoolDayBook/Models/Student.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace HomeSchoolDayBook.Models
 {
@@ -11,7 +12,8 @@ namespace HomeSchoolDayBook.Models
         public int ID { get; set; }
         public string Name { get; set; }
 
-        [Display(Name ="Active?")]
+        [Display(Name ="Active")]
+        [DefaultValue(true)]
         public bool IsActive { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
