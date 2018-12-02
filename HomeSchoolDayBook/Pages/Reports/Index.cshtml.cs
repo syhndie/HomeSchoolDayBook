c0c50fc60f1b2109f6692e2ecbd5d27340ab7fd6
Cindy <syhndie2@gmail.com>
2018-12-02 16:10:14 -0600

    Added link for Create a Report on nav bar. Created a form to enter report specs. (not finished).

---
+++ HomeSchoolDayBook/Pages/Reports/Index.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using HomeSchoolDayBook.Models.ViewModels;
+
+using Microsoft.EntityFrameworkCore;
+using HomeSchoolDayBook.Data;
+using HomeSchoolDayBook.Models;
+
+namespace HomeSchoolDayBook.Pages.Reports
+{
+    public class IndexModel : PageModel
+    {
+        private readonly HomeSchoolDayBook.Data.ApplicationDbContext _context;
+
+        [DataType(DataType.Date)]
+        [Required]
+        [Display(Name ="Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Students")]
+        public List<CheckBoxVM> StudentCheckBoxes { get; set; }
+
+        public IndexModel(HomeSchoolDayBook.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> OnGetAsync()
+        {
+            StartDate = DateTime.Today.AddMonths(-1);
+
+            EndDate = DateTime.Today;
+
+            StudentCheckBoxes = await _context
+                .Students
+                .Where(st => st.IsActive)
+                .OrderBy(st => st.Name)
+                .Select(st => new CheckBoxVM(st.ID, st.Name, false))
+                .ToListAsync();
+
+            return Page();
+            
+        }
+    }
+}
