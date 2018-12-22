07150aa084799ed9ec22cffe325eb0ad1d6a80e0
Cindy <syhndie2@gmail.com>
2018-12-22 14:27:17 -0600

    Changed TimeSpentPerDayVM property TimeSpentLookup to be a Dictionary<Datetime, int?> rather than Dictionary<string, string>. Then added formatting for the Datetime to the TimeSpentPerDay page itself.

--- HomeSchoolDayBook/Models/ViewModels/TimeSpentPerDayVM.cs
+++ HomeSchoolDayBook/Models/ViewModels/TimeSpentPerDayVM.cs
@@ -11,36 +11,18 @@ namespace HomeSchoolDayBook.Models.ViewModels
     {
         public Student Student { get; set; }
 
-        public Dictionary<string, string> TimePerDayLookup { get; set; }
+        public Dictionary<DateTime, int?> TimePerDayLookup { get; set; }
 
         public TimeSpentPerDayVM(ApplicationDbContext context, int studentID, DateTime startDate, DateTime endDate)
         {
             Student = context.Students.FirstOrDefault(m => m.ID == studentID);
 
-            var test = context.Entries
+            TimePerDayLookup = context.Entries
                 .Include(ent => ent.Enrollments)
                 .Where(ent => startDate <= ent.Date && ent.Date <= endDate)
                 .Where(ent => ent.Enrollments.Select(enr => enr.StudentID).Contains(studentID))
                 .GroupBy(ent => ent.Date)
                 .ToDictionary(x => x.Key, x => x.Sum(y => y.MinutesSpent));
-
-            TimePerDayLookup = new Dictionary<string, string>();
-
-            foreach (var day in test)
-            {
-                int? computedHours = day.Value / 60;
-                int? computedMinutes = day.Value % 60;
-                string hoursUnits = computedHours == 1 ? "hour" : "hours";
-                string minutesUnits = computedMinutes == 1 ? "minute" : "minutes";
-                string hours = (computedHours == 0 || computedHours == null) ? "" : $"{computedHours} {hoursUnits}";
-                string minutes = (computedMinutes == 0 || computedMinutes == null) ? "" : $"{computedMinutes} {minutesUnits}";
-                string timeDisplay = (hours == "" || minutes == "") ? $"{hours} {minutes}" : $"{hours}, {minutes}";
-
-                TimePerDayLookup.Add($"{day.Key.ToShortDateString()} ({day.Key.DayOfWeek})", 
-                    (timeDisplay == " " || timeDisplay == null) 
-                    ? "All entries for this day have no time values entered." 
-                    : timeDisplay);
-            }
         }
     }
 }
