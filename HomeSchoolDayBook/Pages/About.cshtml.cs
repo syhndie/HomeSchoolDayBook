c0812fe00b7d74a860836cf9f31111bb201d432c
Cindy <syhndie2@gmail.com>
2019-01-03 16:35:11 -0600

    Added [AllowAnonymous] to Main Index, Contact, and About page models

--- HomeSchoolDayBook/Pages/About.cshtml.cs
+++ HomeSchoolDayBook/Pages/About.cshtml.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HomeSchoolDayBook.Pages
 {
+    [AllowAnonymous]
     public class AboutModel : PageModel
     {
         public string Message { get; set; }
--- HomeSchoolDayBook/Pages/Index.cshtml.cs
+++ HomeSchoolDayBook/Pages/Index.cshtml.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HomeSchoolDayBook.Pages
 {
+    [AllowAnonymous]
     public class IndexModel : PageModel
     {
         public string Message { get; set; }
