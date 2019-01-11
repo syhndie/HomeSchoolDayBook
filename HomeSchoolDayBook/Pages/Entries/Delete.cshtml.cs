1e488e1f2a08481ccba8f78a0acc3440e7a5445f
Cindy <syhndie2@gmail.com>
2019-01-11 14:02:55 -0600

    Entries can only be deleted by their users

--- HomeSchoolDayBook/Pages/Entries/Delete.cshtml.cs
+++ HomeSchoolDayBook/Pages/Entries/Delete.cshtml.cs
@@ -7,11 +7,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using HomeSchoolDayBook.Data;
 using HomeSchoolDayBook.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace HomeSchoolDayBook.Pages.Entries
 {
     public class DeleteModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
         private readonly HomeSchoolDayBook.Data.ApplicationDbContext _context;
 
         public Entry Entry { get; set; }
@@ -19,24 +22,29 @@ namespace HomeSchoolDayBook.Pages.Entries
         [TempData]
         public string NotFoundMessage { get; set; }
 
-        public DeleteModel(HomeSchoolDayBook.Data.ApplicationDbContext context)
+        public DeleteModel(HomeSchoolDayBook.Data.ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
+            _userManager = userManager;
             _context = context;
         }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
+            string userId = _userManager.GetUserId(User);
+
             Entry = await _context.Entries
+                .Where(ent => ent.UserID == userId)
+                .Where(ent => ent.ID == id)
                 .Include(ent => ent.Enrollments)
                     .ThenInclude(enr => enr.Student)
                 .Include(ent => ent.SubjectAssignments)
                     .ThenInclude(sa => sa.Subject)
                 .AsNoTracking()
-                .FirstOrDefaultAsync(m => m.ID == id);
+                .FirstOrDefaultAsync();
 
             if (Entry == null)
             {
-                NotFoundMessage = "Entry not found. The Entry you selected is no longer in the database.";
+                NotFoundMessage = "Entry not found.";
 
                 return RedirectToPage("./Index");
             }
@@ -46,9 +54,13 @@ namespace HomeSchoolDayBook.Pages.Entries
 
         public async Task<IActionResult> OnPostAsync(int? id)
         {
+            string userId = _userManager.GetUserId(User);
+
             Entry = await _context.Entries
+                .Where(ent => ent.UserID == userId)
+                .Where(ent => ent.ID == id)
                 .AsNoTracking()
-                .FirstOrDefaultAsync(e => e.ID == id);
+                .FirstOrDefaultAsync();
 
             if (Entry == null)
             {
