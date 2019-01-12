38f897cdeae67160afe38e9b979ce36fc7106739
Cindy <syhndie2@gmail.com>
2019-01-12 10:16:49 -0600

    Added a message to Report Index page, alerting user if there are no saved Students

--- HomeSchoolDayBook/Pages/Reports/Index.cshtml.cs
+++ HomeSchoolDayBook/Pages/Reports/Index.cshtml.cs
@@ -7,10 +7,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using HomeSchoolDayBook.Models.ViewModels;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 namespace HomeSchoolDayBook.Pages.Reports
 {
     public class IndexModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
         private readonly HomeSchoolDayBook.Data.ApplicationDbContext _context;
 
         public readonly List<string> ReportViews = new List<string>
@@ -27,29 +31,40 @@ namespace HomeSchoolDayBook.Pages.Reports
         [Display(Name = "Students")]
         public List<CheckBoxVM> StudentCheckBoxes { get; set; }
 
-        public IndexModel(HomeSchoolDayBook.Data.ApplicationDbContext context)
+        public string NoStudentsMessage { get; set; }
+
+        public IndexModel(HomeSchoolDayBook.Data.ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
+            _userManager = userManager;
             _context = context;
         }
         
         public async Task<IActionResult> OnGetAsync()
         {
+            string userId = _userManager.GetUserId(User);
+
             FromDate = DateTime.Today.AddMonths(-1);
 
             ToDate = DateTime.Today;
 
             StudentCheckBoxes = await _context
                 .Students
+                .Where(st => st.UserID == userId)
                 .Where(st => st.IsActive)
                 .OrderBy(st => st.Name)
                 .Select(st => new CheckBoxVM(st.ID, st.Name, false))
                 .ToListAsync();
 
+            if (StudentCheckBoxes.Count() == 0)
+            {
+                NoStudentsMessage = "You have no saved Students.";
+            }
+
             return Page();            
         }
 
         public IActionResult OnPost(string fromDate, string toDate, string[] selectedStudents, string selectedReport)
-        { 
+        {
             string startDate = Convert.ToDateTime(fromDate) <= Convert.ToDateTime(toDate) ? fromDate : toDate;
 
             string endDate = startDate == fromDate ? toDate : fromDate;
