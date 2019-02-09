2ca62cb0fbc9baa9f23fb06a6e3ecfae558ce61e
Cindy <syhndie2@gmail.com>
2019-02-09 13:49:56 -0600

    added subjects to EntriesInFull report and changed styling from html table to bootstrap grid

--- HomeSchoolDayBook/Pages/Reports/EntriesInFull.cshtml.cs
+++ HomeSchoolDayBook/Pages/Reports/EntriesInFull.cshtml.cs
@@ -21,7 +21,11 @@ namespace HomeSchoolDayBook.Pages.Reports
 
         public EntriesReportVM EntriesReportVM { get; set; }
 
+        [Display(Name = "Students")]
         public Dictionary<int, string> StudentNameLookup { get; set; }
+
+        [Display(Name = "Subjects")]
+        public Dictionary<int, string> SubjectNameLookup { get; set; }
         
         public EntriesInFullModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
@@ -51,7 +55,7 @@ namespace HomeSchoolDayBook.Pages.Reports
                     }
                     else if (i == entry.Enrollments.Count() - 1)
                     {
-                        studentNames = $"{studentNames} and {enrollments[i].Student.Name}";
+                        studentNames = $"{studentNames}, and {enrollments[i].Student.Name}";
                     }
                     else
                     {
@@ -60,6 +64,33 @@ namespace HomeSchoolDayBook.Pages.Reports
                 }
 
                 StudentNameLookup.Add(entry.ID, studentNames);
+            }
+
+            SubjectNameLookup = new Dictionary<int, string>();
+
+            foreach (Entry entry in EntriesReportVM.Entries)
+            {
+                List<SubjectAssignment> subjectAssignments = entry.SubjectAssignments.OrderBy(sa => sa.Subject.Name).ToList();
+
+                string subjectNames = "";
+
+                for (int i = 0; i < entry.SubjectAssignments.Count(); i++)
+                {
+                    if (i == 0)
+                    {
+                        subjectNames = subjectAssignments[i].Subject.Name;
+                    }
+                    else if (i == entry.SubjectAssignments.Count() - 1)
+                    {
+                        subjectNames = $"{subjectNames}, and {subjectAssignments[i].Subject.Name}";
+                    }
+                    else
+                    {
+                        subjectNames = $"{subjectNames}, {subjectAssignments[i].Subject.Name}";
+                    }
+                }
+
+                SubjectNameLookup.Add(entry.ID, subjectNames);
             }
         }
     }
