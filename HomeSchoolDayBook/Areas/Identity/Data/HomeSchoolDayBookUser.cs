5938d11ec63f364e1add2d10dae6e5921fbc0263
Cindy <syhndie2@gmail.com>
2019-02-17 18:27:21 -0600

    added property to user to track number of confirmation emails sent

--- HomeSchoolDayBook/Areas/Identity/Data/HomeSchoolDayBookUser.cs
+++ HomeSchoolDayBook/Areas/Identity/Data/HomeSchoolDayBookUser.cs
@@ -11,5 +11,6 @@ namespace HomeSchoolDayBook.Areas.Identity.Data
     {
         public string PendingEmail { get; set; }
         public DateTime AccountCreatedTimeStamp { get; set; }
+        public int EmailConfirmsCount { get; set; }
     }
 }
