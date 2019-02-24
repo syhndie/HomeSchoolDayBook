959c0e9ef6fb269cff1a3961c9abb083a329d902
Cindy <syhndie2@gmail.com>
2019-02-23 22:31:40 -0600

    created BasePageModel, inheriting from PageModel

---
+++ HomeSchoolDayBook/Models/BasePageModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HomeSchoolDayBook.Models
+{
+    public class BasePageModel : PageModel
+    {
+        public class Message
+        {
+            public string MessageClass { get; set; }
+            public string MessageText { get; set; }
+        }
+        [TempData]
+        public List<Message> Messages { get; set; }        
+    }
+
+   
+}
