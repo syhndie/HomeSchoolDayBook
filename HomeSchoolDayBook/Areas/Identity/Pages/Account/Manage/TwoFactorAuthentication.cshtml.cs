28aeb582dc77cd36f8a560c9d7e8f1e1f808d666
Cindy <syhndie2@gmail.com>
2019-03-03 15:16:29 -0600

    ensured TwoFactorAuthentication page only shows "not enabled" message if someone manages to find their way to it

--- HomeSchoolDayBook/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
+++ HomeSchoolDayBook/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
@@ -9,61 +9,50 @@ namespace HomeSchoolDayBook.Areas.Identity.Pages.Account.Manage
 {
     public class TwoFactorAuthenticationModel : BasePageModel
     {
-        private const string AuthenicatorUriFormat = "otpauth://totp/{0}:{1}?secret={2}&issuer={0}";
+        //private const string AuthenicatorUriFormat = "otpauth://totp/{0}:{1}?secret={2}&issuer={0}";
 
-        private readonly UserManager<HomeSchoolDayBookUser> _userManager;
-        private readonly SignInManager<HomeSchoolDayBookUser> _signInManager;
-        private readonly ILogger<TwoFactorAuthenticationModel> _logger;
+        //private readonly UserManager<HomeSchoolDayBookUser> _userManager;
+        //private readonly SignInManager<HomeSchoolDayBookUser> _signInManager;
+        //private readonly ILogger<TwoFactorAuthenticationModel> _logger;
 
-        public TwoFactorAuthenticationModel(
-            UserManager<HomeSchoolDayBookUser> userManager,
-            SignInManager<HomeSchoolDayBookUser> signInManager,
-            ILogger<TwoFactorAuthenticationModel> logger)
+        //public TwoFactorAuthenticationModel(
+        //    UserManager<HomeSchoolDayBookUser> userManager,
+        //    SignInManager<HomeSchoolDayBookUser> signInManager,
+        //    ILogger<TwoFactorAuthenticationModel> logger)
+        //{
+        //    _userManager = userManager;
+        //    _signInManager = signInManager;
+        //    _logger = logger;
+        //}
+
+        //public bool HasAuthenticator { get; set; }
+
+        //public int RecoveryCodesLeft { get; set; }
+
+        //[BindProperty]
+        //public bool Is2faEnabled { get; set; }
+
+        //public bool IsMachineRemembered { get; set; }
+
+        //[TempData]
+        //public string StatusMessage { get; set; }
+
+        public void OnGet()
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _logger = logger;
+
         }
 
-        public bool HasAuthenticator { get; set; }
+        //public async Task<IActionResult> OnPost()
+        //{
+            //var user = await _userManager.GetUserAsync(User);
+            //if (user == null)
+            //{
+            //    return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            //}
 
-        public int RecoveryCodesLeft { get; set; }
-
-        [BindProperty]
-        public bool Is2faEnabled { get; set; }
-
-        public bool IsMachineRemembered { get; set; }
-
-        [TempData]
-        public string StatusMessage { get; set; }
-
-        public async Task<IActionResult> OnGet()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            HasAuthenticator = await _userManager.GetAuthenticatorKeyAsync(user) != null;
-            Is2faEnabled = await _userManager.GetTwoFactorEnabledAsync(user);
-            IsMachineRemembered = await _signInManager.IsTwoFactorClientRememberedAsync(user);
-            RecoveryCodesLeft = await _userManager.CountRecoveryCodesAsync(user);
-
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPost()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            await _signInManager.ForgetTwoFactorClientAsync();
-            StatusMessage = "The current browser has been forgotten. When you login again from this browser you will be prompted for your 2fa code.";
-            return RedirectToPage();
-        }
+            //await _signInManager.ForgetTwoFactorClientAsync();
+            //StatusMessage = "The current browser has been forgotten. When you login again from this browser you will be prompted for your 2fa code.";
+        //    return RedirectToPage();
+        //}
     }
 }
