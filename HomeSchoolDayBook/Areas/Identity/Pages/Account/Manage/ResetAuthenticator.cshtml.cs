25ec816f5ab83719fb7d332f4c50c444e806354d
Cindy <syhndie2@gmail.com>
2019-03-06 09:51:14 -0600

    ensured ResetAuthenticator page only shows "not enabled" message

--- HomeSchoolDayBook/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
+++ HomeSchoolDayBook/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
@@ -9,50 +9,50 @@ namespace HomeSchoolDayBook.Areas.Identity.Pages.Account.Manage
 {
     public class ResetAuthenticatorModel : BasePageModel
     {
-        UserManager<HomeSchoolDayBookUser> _userManager;
-        private readonly SignInManager<HomeSchoolDayBookUser> _signInManager;
-        ILogger<ResetAuthenticatorModel> _logger;
+        //UserManager<HomeSchoolDayBookUser> _userManager;
+        //private readonly SignInManager<HomeSchoolDayBookUser> _signInManager;
+        //ILogger<ResetAuthenticatorModel> _logger;
 
-        public ResetAuthenticatorModel(
-            UserManager<HomeSchoolDayBookUser> userManager,
-            SignInManager<HomeSchoolDayBookUser> signInManager,
-            ILogger<ResetAuthenticatorModel> logger)
+        //public ResetAuthenticatorModel(
+        //    UserManager<HomeSchoolDayBookUser> userManager,
+        //    SignInManager<HomeSchoolDayBookUser> signInManager,
+        //    ILogger<ResetAuthenticatorModel> logger)
+        //{
+        //    _userManager = userManager;
+        //    _signInManager = signInManager;
+        //    _logger = logger;
+        //}
+
+        //[TempData]
+        //public string StatusMessage { get; set; }
+
+        public void OnGet()
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _logger = logger;
+            //var user = await _userManager.GetUserAsync(User);
+            //if (user == null)
+            //{
+            //    return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            //}
+
+            //return Page();
         }
 
-        [TempData]
-        public string StatusMessage { get; set; }
+    //    public async Task<IActionResult> OnPostAsync()
+    //    {
+    //        var user = await _userManager.GetUserAsync(User);
+    //        if (user == null)
+    //        {
+    //            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+    //        }
 
-        public async Task<IActionResult> OnGet()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
+    //        await _userManager.SetTwoFactorEnabledAsync(user, false);
+    //        await _userManager.ResetAuthenticatorKeyAsync(user);
+    //        _logger.LogInformation("User with ID '{UserId}' has reset their authentication app key.", user.Id);
 
-            return Page();
-        }
+    //        await _signInManager.RefreshSignInAsync(user);
+    //        StatusMessage = "Your authenticator app key has been reset, you will need to configure your authenticator app using the new key.";
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            await _userManager.SetTwoFactorEnabledAsync(user, false);
-            await _userManager.ResetAuthenticatorKeyAsync(user);
-            _logger.LogInformation("User with ID '{UserId}' has reset their authentication app key.", user.Id);
-
-            await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Your authenticator app key has been reset, you will need to configure your authenticator app using the new key.";
-
-            return RedirectToPage("./EnableAuthenticator");
-        }
+    //        return RedirectToPage("./EnableAuthenticator");
+    //    }
     }
 }
