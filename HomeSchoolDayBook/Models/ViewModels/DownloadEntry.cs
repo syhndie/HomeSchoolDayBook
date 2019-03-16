be09cb15cdc614363fc0e923d63a0fbca1ca2463
Cindy <syhndie2@gmail.com>
2019-03-16 10:33:01 -0500

    added a DownloadEntry class to contain all fields of an entry, including all students and subjects, that the user would download

---
+++ HomeSchoolDayBook/Models/ViewModels/DownloadEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeSchoolDayBook.Models;
+
+namespace HomeSchoolDayBook.Models.ViewModels
+{
+    public class DownloadEntry
+    {
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        public int? MinutesSpent { get; set; }
+
+        public string Students { get; set; }
+
+        public string Subjects { get; set; }
+
+        public DownloadEntry(Entry entry, string students, string subjects)
+        {
+            Title = entry.Title;
+            Date = entry.Date;
+            Description = entry.Description;
+            MinutesSpent = entry.MinutesSpent;
+            Students = students;
+            Subjects = subjects;
+        }
+    }
+}
