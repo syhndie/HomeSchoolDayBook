a55d9592cc0bb3292d523f75e2ad9fa3f70a5dfa
Cindy <syhndie2@gmail.com>
2019-03-16 11:07:10 -0500

    using Stringbuilder in GetStudentNames and GetSubjectNames methods

--- HomeSchoolDayBook/Helpers/Helpers.cs
+++ HomeSchoolDayBook/Helpers/Helpers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HomeSchoolDayBook.Models;
+using System.Text;
 
 namespace HomeSchoolDayBook.Helpers
 {
@@ -30,49 +31,49 @@ namespace HomeSchoolDayBook.Helpers
         {
             List<Enrollment> enrollments = entry.Enrollments.OrderBy(enr => enr.Student.Name).ToList();
 
-            string studentNames = "";
+            StringBuilder studentNames = new StringBuilder();
 
             for (int i = 0; i < entry.Enrollments.Count(); i++)
             {
                 if (i == 0)
                 {
-                    studentNames = enrollments[i].Student.Name;
+                    studentNames.Append(enrollments[i].Student.Name);
                 }
                 else if (i == entry.Enrollments.Count() - 1)
                 {
-                    studentNames = $"{studentNames} and {enrollments[i].Student.Name}";
+                    studentNames.Append($" and {enrollments[i].Student.Name}");
                 }
                 else
                 {
-                    studentNames = $"{studentNames}, {enrollments[i].Student.Name}";
+                    studentNames.Append($", {enrollments[i].Student.Name}");
                 }
             }
-            return studentNames;
+            return studentNames.ToString();
         }
 
         public static string GetSubjectNames(Entry entry)
         {
             List<SubjectAssignment> subjectAssignments = entry.SubjectAssignments.OrderBy(sa => sa.Subject.Name).ToList();
 
-            string subjectNames = "";
+            StringBuilder subjectNames = new StringBuilder();
 
             for (int i = 0; i < entry.SubjectAssignments.Count(); i++)
             {
                 if (i == 0)
                 {
-                    subjectNames = subjectAssignments[i].Subject.Name;
+                    subjectNames.Append(subjectAssignments[i].Subject.Name);
                 }
                 else if (i == entry.SubjectAssignments.Count() - 1)
                 {
-                    subjectNames = $"{subjectNames} and {subjectAssignments[i].Subject.Name}";
+                    subjectNames.Append($" and {subjectAssignments[i].Subject.Name}");
                 }
                 else
                 {
-                    subjectNames = $"{subjectNames}, {subjectAssignments[i].Subject.Name}";
+                    subjectNames.Append($", {subjectAssignments[i].Subject.Name}");
                 }
             }
 
-            return subjectNames;
+            return subjectNames.ToString();
         }
 
     }
