9020e371f8f07fd6214c3b71e4e1064636551ecc
Cindy <syhndie2@gmail.com>
2019-03-22 18:20:18 -0500

    Revert "DownloadData page now downloads ell entries for a user to the client"
    This reverts commit 23831917c2d92951dd10ad9d59eef32644a46067.

--- HomeSchoolDayBook/Areas/Identity/Pages/Account/Manage/DownloadData.cshtml.cs
+++ HomeSchoolDayBook/Areas/Identity/Pages/Account/Manage/DownloadData.cshtml.cs
@@ -1,10 +1,10 @@
 ﻿using System.Threading.Tasks;
-using System;
 using HomeSchoolDayBook.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using HomeSchoolDayBook.Models;
+using HomeSchoolDayBook.Models.ViewModels;
 using HomeSchoolDayBook.Data;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
@@ -12,15 +12,11 @@ using System.Collections.Generic;
 using static HomeSchoolDayBook.Helpers.Helpers;
 using System.IO;
 using CsvHelper;
-using System.Text;
 
 namespace HomeSchoolDayBook.Areas.Identity.Pages.Account.Manage
 {
     public class DownloadDataModel : BasePageModel
     {
-        private readonly static string DateStamp = DateTime.Now.ToString("yyyyMMdd");
-        private readonly string FileName = $"HSDBData{DateStamp}.csv";
-
         private readonly ApplicationDbContext _context;
         private readonly UserManager<HomeSchoolDayBookUser> _userManager;
         private readonly ILogger<DownloadDataModel> _logger;
@@ -49,7 +45,23 @@ namespace HomeSchoolDayBook.Areas.Identity.Pages.Account.Manage
                 return RedirectToPage();
             }
 
-            _logger.LogInformation("User with ID '{UserId}' asked for data download.", _userManager.GetUserId(User));     
+            _logger.LogInformation("User with ID '{UserId}' asked for data download.", _userManager.GetUserId(User));
+
+            //use csvhelper
+            //this may allow me to just have a list of downloadEntries and it will write based on the properties.
+            //try that first.
+            //if not, I might need to make a list of string arrays
+
+            //make each entry into an array of strings, one string for each column
+            string[] columnHeaders = new string[]
+            {
+                "Title",
+                "Date",
+                "Description",
+                "Minutes Spent",
+                "Students",
+                "Subjects"
+            };
 
             string userId = _userManager.GetUserId(User);
              
@@ -62,27 +74,13 @@ namespace HomeSchoolDayBook.Areas.Identity.Pages.Account.Manage
                .AsNoTracking()
                .ToListAsync();
 
-            var stringWriter = new StringWriter();
-            var csvWriter = new CsvWriter(stringWriter);
-
-            using (csvWriter)
+            //write to the csv file as you loop through the entries
+            using (var writer = new StreamWriter(@"C:\Users\Cindy\Documents\Visual Studio 2017\Projects\HomeSchoolDayBook\HomeSchoolDayBook\wwwroot\HSDBData.csv"))
+            using (var csv = new CsvWriter(writer))
             {
-                var columnHeader = new
-                {
-                    Title = "Title",
-                    Date = "Date",
-                    Description = "Description",
-                    Minutes = "Minutes Spent",
-                    Students = "Students",
-                    Subjects = "Subjects"
-                };
-
-                csvWriter.WriteRecord(columnHeader);
-                csvWriter.NextRecord();
-
                 foreach (Entry entry in entries)
                 {
-                    var record = new
+                    var download = new
                     {
                         Date = entry.Date.ToShortDateString(),
                         Title = entry.Title,
@@ -92,13 +90,32 @@ namespace HomeSchoolDayBook.Areas.Identity.Pages.Account.Manage
                         Subjects = GetSubjectNames(entry)
                     };
 
-                    csvWriter.WriteRecord(record);
-                    csvWriter.NextRecord();
+                    csv.WriteRecord(download);
+                    csv.NextRecord();
                 }
+
             }
 
-            var bytes = Encoding.UTF8.GetBytes(stringWriter.ToString());
-            return File(bytes, "application/octet-stream", FileName);
+
+            //        void Main()
+            //        {
+            //            var records = new List<object>
+            //{
+            //    new { Id = 1, Name = "one" },
+            //};
+
+            //            using (var writer = new StreamWriter("path\\to\\file.csv"))
+            //            using (var csv = new CsvWriter(writer))
+            //            {
+            //                csv.WriteRecords(records);
+            //            }
+            //        }
+
+
+
+
+
+            return RedirectToPage();
         }
     }
 }
