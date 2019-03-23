6ae9dd18c57198fa22cb7f68692d0f3541e09e4f
Cindy <syhndie2@gmail.com>
2019-03-23 15:38:22 -0500

    Added Grade class to database wtih foreign keys of entryid, studentid, subjectid

---
+++ HomeSchoolDayBook/Data/Migrations/20190323143305_AddedGrades.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace HomeSchoolDayBook.Data.Migrations
+{
+    public partial class AddedGrades : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Grade",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    EntryID = table.Column<int>(nullable: false),
+                    StudentID = table.Column<int>(nullable: false),
+                    SubjectID = table.Column<int>(nullable: false),
+                    PointsEarned = table.Column<int>(nullable: false),
+                    PointsAvailable = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grade", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Grade_Entries_EntryID",
+                        column: x => x.EntryID,
+                        principalTable: "Entries",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Grade_Students_StudentID",
+                        column: x => x.StudentID,
+                        principalTable: "Students",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Grade_Subjects_SubjectID",
+                        column: x => x.SubjectID,
+                        principalTable: "Subjects",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grade_EntryID",
+                table: "Grade",
+                column: "EntryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grade_StudentID",
+                table: "Grade",
+                column: "StudentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grade_SubjectID",
+                table: "Grade",
+                column: "SubjectID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grade");
+        }
+    }
+}
---
+++ HomeSchoolDayBook/Models/Grade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeSchoolDayBook.Models
+{
+    public class Grade
+    {
+        public int ID { get; set; }
+        public int EntryID { get; set; }
+        public int StudentID { get; set; }
+        public int SubjectID { get; set; }
+        public int PointsEarned { get; set; }
+        public int PointsAvailable { get; set; }
+
+        public Entry Entry { get; set; }
+        public Student Student { get; set; }
+        public Subject Subject { get; set; }
+
+        public decimal Percent
+        {
+            get
+            {
+                return PointsEarned / PointsAvailable;
+            }
+        }
+    }
+}
