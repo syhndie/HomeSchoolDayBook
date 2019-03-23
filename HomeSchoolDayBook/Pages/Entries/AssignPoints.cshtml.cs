841968331d48700154d4238f8b6c0be98fe18fd5
Cindy <syhndie2@gmail.com>
2019-03-23 15:41:04 -0500

    created an AssignPoints page and pagemodel, with links from the Entries Index page

---
+++ HomeSchoolDayBook/Pages/Entries/AssignPoints.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HomeSchoolDayBook.Data;
+using HomeSchoolDayBook.Models;
+using Microsoft.AspNetCore.Identity;
+using HomeSchoolDayBook.Areas.Identity.Data;
+
+namespace HomeSchoolDayBook.Pages.Entries
+{
+    public class AssignPointsModel : BasePageModel
+    {
+        private readonly UserManager<HomeSchoolDayBookUser> _userManager;
+
+        private readonly ApplicationDbContext _context;
+
+        public Entry Entry { get; set; }
+
+        public AssignPointsModel(ApplicationDbContext context, UserManager<HomeSchoolDayBookUser> userManager)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            string userId = _userManager.GetUserId(User);
+
+            Entry = await _context.Entries
+                .Where(ent => ent.UserID == userId)
+                .Where(ent => ent.ID == id)
+                .Include(ent => ent.Enrollments)
+                    .ThenInclude(enr => enr.Student)
+                .Include(ent => ent.SubjectAssignments)
+                    .ThenInclude(sa => sa.Subject)
+                .Include(ent => ent.Grades)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+
+            if (Entry == null)
+            {
+                DangerMessage = "Entry not found.";
+
+                return RedirectToPage("./Index");
+            }
+
+
+
+            return Page();
+        }
+    }
+}
