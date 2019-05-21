2c21af5797f4f57304ba9859053ad268006fdbaa
Cindy <syhndie2@gmail.com>
2019-05-21 15:17:34 -0500

    created page model for SubjectGrades report, including a list of StudentSubjectGradeVM

---
+++ HomeSchoolDayBook/Models/ViewModels/StudentSubjectGradeVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeSchoolDayBook.Models.ViewModels
+{
+    public class StudentSubjectGradeVM
+    {
+        [Display(Name = "Student")]
+        public string StudentName { get; set; }
+
+        [Display(Name = "Subject")]
+        public string SubjectName { get; set; }
+
+        [Display(Name = "Points Earned")]
+        public decimal PointsEarned { get; set; }
+
+        [Display(Name = "Points Available")]
+        public decimal PointsAvailable { get; set; }
+
+        [Display(Name = "Percent Earned")]
+        [DisplayFormat(DataFormatString = "{0:P2}")]
+        public decimal PercentEarned
+        {
+            get
+            {
+                return PointsEarned / PointsAvailable;
+            }
+        }
+
+        public StudentSubjectGradeVM(string studentName, string subjectName, decimal pointsEarned, decimal pointsAvailable)
+        {
+            StudentName = studentName;
+            SubjectName = subjectName;
+            PointsEarned = pointsEarned;
+            PointsAvailable = pointsAvailable;
+        }
+    }
+}
--- HomeSchoolDayBook/Pages/Reports/SubjectGrades.cshtml.cs
+++ HomeSchoolDayBook/Pages/Reports/SubjectGrades.cshtml.cs
@@ -1,17 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using HomeSchoolDayBook.Data;
+using System.ComponentModel.DataAnnotations;
+using HomeSchoolDayBook.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using HomeSchoolDayBook.Areas.Identity.Data;
+using HomeSchoolDayBook.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HomeSchoolDayBook.Pages.Reports
 {
-    public class SubjectGradesModel : PageModel
+    public class SubjectGradesModel : BasePageModel
     {
-        public void OnGet()
-        {
+        private readonly UserManager<HomeSchoolDayBookUser> _userManager;
 
+        private readonly ApplicationDbContext _context;
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        public List<StudentSubjectGradeVM> StudentGradesBySubject { get; set; }      
+
+        public SubjectGradesModel(ApplicationDbContext context, UserManager<HomeSchoolDayBookUser> userManager)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public void OnGet(string start, string end, string studentIDs)
+        {
+            string userId = _userManager.GetUserId(User);
+
+            StartDate = Convert.ToDateTime(start);
+
+            EndDate = Convert.ToDateTime(end);
+
+            if (studentIDs == null || studentIDs == "") StudentGradesBySubject = new List<StudentSubjectGradeVM>();
+            else
+            {
+                List<int> studentIDList = studentIDs.Split(',')
+                    .Select(int.Parse)
+                    .ToList();
+                
+                StudentGradesBySubject = _context.Grades
+                    .Where(gr => gr.UserID == userId)
+                    .Include(gr => gr.Student)
+                    .Include(gr => gr.Subject)
+                    .Where(gr => StartDate <= gr.Entry.Date)
+                    .Where(gr => gr.Entry.Date <= EndDate)
+                    .Where(gr => studentIDList.Contains(gr.StudentID)).ToList()
+                    .GroupBy(gr => new { gr.Student, gr.Subject }).ToList()
+                    .Select(x => new StudentSubjectGradeVM(x.Key.Student.Name, x.Key.Subject.Name, x.Sum(y => y.PointsEarned), x.Sum(y => y.PointsAvailable)))                    
+                    .ToList();
+            }
         }
     }
 }
