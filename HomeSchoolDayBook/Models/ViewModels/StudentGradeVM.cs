8b48e324c60abedc6224cf479028046b0fb4f14d
Cindy <syhndie2@gmail.com>
2019-06-13 17:03:10 -0500

    added SubjectGradeSummary page and  StudentGradeVM to suupert it.  also renamed Student Grades page to StudentGradeSummary

---
+++ HomeSchoolDayBook/Models/ViewModels/StudentGradeVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeSchoolDayBook.Models.ViewModels
+{
+    public class StudentGradeVM
+    {
+        public Student Student { get; set; }
+
+        [Display(Name = "Points Earned")]
+        public decimal PointsEarned { get; set; }
+
+        [Display(Name = "Points Available")]
+        [Range(double.Epsilon, double.MaxValue)]
+        public decimal PointsAvailable { get; set; }
+
+        [Display(Name = "Percent Earned")]
+        [DisplayFormat(DataFormatString = "{0:P2}")]
+        public decimal PercentEarned
+        {
+            get
+            {
+                return PointsEarned / PointsAvailable;
+            }
+        }
+
+        public StudentGradeVM(Student student, decimal pointsEarned, decimal pointsAvailable)
+        {
+            Student = student;
+            PointsEarned = pointsEarned;
+            PointsAvailable = pointsAvailable;
+        }
+    }
+}
---
+++ HomeSchoolDayBook/Pages/Grades/SubjectGradeSummary.cshtml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HomeSchoolDayBook.Models;
+using Microsoft.AspNetCore.Identity;
+using HomeSchoolDayBook.Data;
+using HomeSchoolDayBook.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HomeSchoolDayBook.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HomeSchoolDayBook.Pages.Grades
+{
+    public class SubjectGradeSummaryModel : BasePageModel
+    {
+        private readonly UserManager<HomeSchoolDayBookUser> _userManager;
+
+        private readonly ApplicationDbContext _context;
+
+        [BindProperty (SupportsGet = true)]
+        [DataType(DataType.Date)]
+        [Display(Name = "From")]
+        public DateTime FromDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        [DataType(DataType.Date)]
+        [Display(Name = "To")]
+        public DateTime ToDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        [Display(Name = "Subject")]
+        public int SubjectID { get; set; }
+
+        public List<SelectListItem> SubjectOptions { get; set; }
+
+        public List<StudentGradeVM> StudentGrades { get; set; }
+
+        public SubjectGradeSummaryModel(ApplicationDbContext context, UserManager<HomeSchoolDayBookUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> OnGetAsync(DateTime? fromDate, DateTime? toDate, int subjectID)
+        {
+            string userId = _userManager.GetUserId(User);
+
+            if (fromDate == null) FromDate = DateTime.Today.AddMonths(-1);
+
+            if (toDate == null) ToDate = DateTime.Today;
+
+            SubjectOptions = await _context
+                .Subjects
+                .Where(su => su.UserID == userId)
+                .Where(su => su.IsActive)
+                .OrderBy(su => su.Name)
+                .Select(su => new SelectListItem { Value = su.ID.ToString(), Text = su.Name })
+                .ToListAsync();
+
+            DateTime startDate = FromDate <= ToDate ? (DateTime)FromDate : (DateTime)ToDate;
+            DateTime endDate = startDate == (DateTime)FromDate ? (DateTime)ToDate : (DateTime)FromDate;
+
+            StudentGrades = await _context
+                .Grades
+                .Where(gr => gr.UserID == userId)
+                .Where(gr => gr.SubjectID == subjectID)
+                .Where(gr => gr.Entry.Date >= startDate)
+                .Where(gr => gr.Entry.Date <= endDate)
+                .GroupBy(gr => gr.Student)
+                .Select(x => new StudentGradeVM(x.Key, x.Sum(y => y.PointsEarned), x.Sum(y => y.PointsAvailable)))
+                .ToListAsync();
+
+            return Page();
+        }
+    }
+}
