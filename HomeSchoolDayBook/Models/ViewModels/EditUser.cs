549d851e2a4ccd9ffd65c205d090373d557c0d97
Cindy <syhndie2@gmail.com>
2019-08-01 15:21:53 -0500

    created a viewmodel to wrap user properties to edit

---
+++ HomeSchoolDayBook/Models/ViewModels/EditUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeSchoolDayBook.Models.ViewModels
+{
+    public class EditUser
+    {
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        /// <summary>datetime account was created</summary>
+        [DataType(DataType.DateTime)]
+        public DateTime AccountCreatedTimeStamp { get; set; }
+
+        /// <summary>number of emails sent to confirm email address, is reset to zero on confirmation</summary>
+        public int EmailConfirmsCount { get; set; }
+
+        /// <summary>number of emails sent to change password, is reset to zero on password change</summary>
+        public int ForgotPasswordEmailCount { get; set; }
+
+        ///<summary>this is not null when a user has requested to change email, but has not yet confirmed the new email address</summary>
+        [DataType(DataType.EmailAddress)]
+        public string PendingEmail { get; set; }
+
+        public EditUser(string email, DateTime accountCreated, bool emailConfirmed, int emailConfirmsCount, int forgotPasswordEmailsCount, string pendingEmail)
+        {
+            Email = email;
+            AccountCreatedTimeStamp = accountCreated;
+            EmailConfirmed = emailConfirmed;
+            EmailConfirmsCount = emailConfirmsCount;
+            ForgotPasswordEmailCount = forgotPasswordEmailsCount;
+            PendingEmail = pendingEmail;
+        }
+    }
+}
--- HomeSchoolDayBook/Pages/Admin/Edit.cshtml.cs
+++ HomeSchoolDayBook/Pages/Admin/Edit.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using HomeSchoolDayBook.Models;
+using HomeSchoolDayBook.Models.ViewModels;
 using HomeSchoolDayBook.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using HomeSchoolDayBook.Data;
@@ -21,24 +22,7 @@ namespace HomeSchoolDayBook.Pages.Admin
     {
         private readonly ApplicationDbContext _context;
 
-        [DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
-
-        public bool EmailConfirmed { get; set; }
-
-        /// <summary>datetime account was created</summary>
-        [DataType(DataType.DateTime)]
-        public DateTime AccountCreatedTimeStamp { get; set; }
-
-        /// <summary>number of emails sent to confirm email address, is reset to zero on confirmation</summary>
-        public int EmailConfirmsCount { get; set; }
-
-        /// <summary>number of emails sent to change password, is reset to zero on password change</summary>
-        public int ForgotPasswordEmailCount { get; set; }
-
-        ///<summary>this is not null when a user has requested to change email, but has not yet confirmed the new email address</summary>
-        [DataType(DataType.EmailAddress)]
-        public string PendingEmail { get; set; }
+        public EditUser EditUser { get; set; }
 
         public EditModel(ApplicationDbContext context)
         {
@@ -56,15 +40,29 @@ namespace HomeSchoolDayBook.Pages.Admin
                 return RedirectToPage("./Index");
             }
 
-            Email = userToEdit.Email;
-            AccountCreatedTimeStamp = userToEdit.AccountCreatedTimeStamp;
-            EmailConfirmed = userToEdit.EmailConfirmed;
+            EditUser = new EditUser
+            (
+                userToEdit.Email,
+                userToEdit.AccountCreatedTimeStamp,
+                userToEdit.EmailConfirmed,
+                userToEdit.EmailConfirmsCount,
+                userToEdit.ForgotPasswordEmailsCount,
+                userToEdit.PendingEmail
+                );
 
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(string userToEditId)
         {
+            HomeSchoolDayBookUser userToEdit = await _context.Users.Where(u => u.Id == userToEditId).SingleOrDefaultAsync();
+
+            if (userToEdit == null)
+            {
+                DangerMessage = "User not found.";
+
+                return RedirectToPage("./Index");
+            }
             return Page();          
         }
     }
